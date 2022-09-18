e5c512e4c79621f55cab4397b966d922523af213
therogersak <therogersak@gmail.com>
2022-09-18 21:20:36 +0530

    optional chaining

---
+++ optionalChaining.js
@@ -0,0 +1,10 @@
+// Optional Chaining
+
+/// Allows you to read the value of a deeply nested property without checking if it's a valid chain
+
+// old method
+const hasValidPostcode = (u) =>
+  u && u.address && u.address.passcode && u.address.passcode.valid;
+
+// new method
+const hasValidPostcode1 = (u) => u?.address?.passcode?.valid;
