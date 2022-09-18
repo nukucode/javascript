8284971e8ffd60463ca5f628541aed9c112c844b
therogersak <therogersak@gmail.com>
2022-09-18 21:17:34 +0530

    js one line codes

--- amazing-js-one-liners.js
+++ amazing-js-one-liners.js
@@ -38,7 +38,45 @@ console.log(slectedText());
 
 function foo() {
   const selObj = window.getSelection();
-  alert(selObj);
+  console.log(selObj);
 }
 
 foo();
+
+/// Append an array to another way
+
+/// if your arrays are not huge, you can use the push() method of the array to which you want to add values
+
+/// the push() method can take multiple parameters so you can use the apply() method to pass the array to be pushed as a collection of function parameters.
+
+let arr1 = [12, 3, 4, 5, "Hello", "Ankit", "45", "47"];
+let arr2 = [234, 45, 5, 665, 76, 678, "Hello", "Manish", 48];
+
+Array.prototype.push.apply(arr1, arr2);
+
+console.log(arr1); // arr1 will be equal to
+console.log(arr2);
+
+/// Verify that a given arguments is a number
+
+function numChecker(n) {
+  return !isNaN(parseFloat(n) && isFinite(n));
+}
+console.log(numChecker(5));
+
+/// cONVERT THE ARGUMENTS OBJECT TO AN ARRAY
+
+//// the boject arguments is not an array. it is similar to an array, but does not have any array properties except length
+
+//// however, it can be converted to a real array using the Array.prototype.slice
+
+function myFunction(arguments) {
+  return Array.prototype.slice.call(arguments);
+}
+
+const obj = {
+  name: "Ankit",
+  age: 18,
+  WORK: "SOFTWARE ENGINEER",
+};
+console.log(myFunction(obj));
