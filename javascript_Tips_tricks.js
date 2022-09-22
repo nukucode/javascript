6f72dae8e69cf24fafd1cd8afd2b697bbeb6336b
therogersak <therogersak@gmail.com>
2022-09-22 19:46:47 +0530

    tips tricks js

---
+++ javascript_Tips_tricks.js
@@ -0,0 +1,11 @@
+
+
+
+//! 1. Javascript Tip:
+
+//* you can wrap your console.log() arguments with curly brackets to see the variable names.
+
+const num = 12345678;
+
+console.log(num)
+console.log({num})
