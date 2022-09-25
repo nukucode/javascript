76c4e95fc232c359cb2ac2d34341dd1d0b05787c
therogersak <therogersak@gmail.com>
2022-09-25 05:54:09 +0530

    tip 2

--- javascript_Tips_tricks.js
+++ javascript_Tips_tricks.js
@@ -1,11 +1,16 @@
-
-
-
 //! 1. Javascript Tip:
 
 //* you can wrap your console.log() arguments with curly brackets to see the variable names.
 
 const num = 12345678;
 
-console.log(num)
-console.log({num})
+console.log(num);
+console.log({ num });
+
+//! Javascript Tip
+
+// you can remove duplicate elements from an array
+
+const cars = ["TATA", "TATA", "AUDI", "BMW", "AUDI"];
+
+console.log(...new Set(cars)); // TATA, BMW, AUDI
