fc28d8e21ca9bb8ed9dd92da19b7f9ea8d246f18
therogersak <therogersak@gmail.com>
2022-09-30 21:15:51 +0530

    :heavy_plus_sign: operators in js
    javascript operators

---
+++ Operators.js
@@ -0,0 +1,83 @@
+//* Javascript Operators
+
+// An Operator is capable of manipulating a certain value or operand. Operators are used to perform specific mathmatical and locial computations on operrand. In other words, we can say that an operator operates the operands.
+
+// -> Operands (x,y) and operator(+)
+
+//? Operators Type
+
+// 1. Aritmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Locial Operators
+// 5. Conditional Operators
+// 6. Type Operators
+// 7. Bitwise Operaotors
+
+// -> ARTMETIC OPERATORS
+
+// + ADDITION
+// - SUBTRACTION
+// * MULTIPLICTION
+// ** EXPONENTIATION (ES72016)
+// / DIVISION
+// % MODULUS (REMAINDER)
+// ++ INCREMENT
+// -- DECREMENT
+
+// -> ASSIGNMENT OPERATORS
+
+// =   X=Y
+// +=  x+=y
+// -=  x-=y
+// /=  x/=y
+// %=  x%=y
+// **= x**=y
+
+//? Shift Assignment Operators
+
+// <<=  x<<=y  - left shift
+// >>=  x>>=y  - right shift
+// >>>= x>>>=y  - unsigned right shift
+
+// -> Locial Assignment Operators
+
+// &=  x&=y
+// ^=  x^=y
+// |=  x|=y
+
+
+
+// -> Comparison Operator
+
+//  == equal to
+//  === equal value and type
+//  !=  not equal
+//  !=== not equal value and type
+// > grater then
+// < less then
+// >= grater then or equal
+// <= less then or equal
+// ? ternary operator
+
+
+
+// -> Locial Operator
+
+// && Logical and
+// || logical or
+// ! logical not
+
+
+// -> Type Operator
+
+// typeof - return the type of a varibles 
+// instanceOf - return true if an object is an instance of an object type.
+
+
+// -> Bitwise Operator
+
+// &  - And
+// | - Or
+// ~  - NON
+// ^ - XOR
