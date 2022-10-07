6db0e7ed7350f5dc3297a3ac76c885051c39f3b5
therogersak <therogersak@gmail.com>
2022-10-07 15:34:11 +0530

    🔖 IIFE

---
+++ iife.js
@@ -0,0 +1,13 @@
+//* Immediately Invoked function expression (IIFE)
+
+// => are js function defined as expression that are immedietely invoked and executed as soon as they are defined
+
+//?? => variables declared within an IIFE are not accessable externally
+
+//** */ => IIFE is used for Imedite code execution and the resulting data privacy
+
+//! Example
+
+(function () {
+  console.log("Hare Krishan");
+})();
