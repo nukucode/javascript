da001acfce0b4fba0618828b2619522aad37dedd
therogersak <therogersak@gmail.com>
2022-10-08 15:51:24 +0530

    🔖 dom

---
+++ Dom/Events/onevents.js
@@ -0,0 +1,29 @@
+//* Different Events and what they do =>>
+
+//! Mouse Events
+
+//  onClick - When mouse click on an element
+//  onmouseover - When the cursor the mouse comes over the element
+//  onmouseout  - When the cursor of the mouse leaves an element
+//  onmousedown - When the mouse button is pressed over the element
+//  onmouseup - When the mouse button is released over thr element
+// onmousemove - when the mouse movement takes place.
+
+//! Keybord Events
+
+// onKeydown - When the user press and then release the key & onKeyup
+
+//! Form Events
+
+// onfocus - when the user focuses on an element
+// onblur  - When the focus is away from a from element
+// onsubmit - When the user submits the form
+// onchange - When the user modifies or change the value of a form element
+
+//! Window Events
+
+// onload - When the browser finishes the loading of the page
+
+// onunload - When the visitor leaves the current webPage, the brower unload it
+
+// onresize - When the visitor resizes the window of the browser
