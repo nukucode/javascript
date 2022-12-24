f1e7c49424e42a6c43bfbd00d1e75a8248c1b1db
therogersak <therogersak@gmail.com>
2022-12-24 18:33:29 +0530

    ✅ Challengers

--- Challenges/Function/Challenges.js
+++ Challenges/Function/Challenges.js
@@ -156,7 +156,7 @@ function sampler(fn, count) {
 
 => When requested multiple times, same object returned*/
 
-var Singleton = function () {
+var Singleton = (function () {
   var instance;
 
   function createInstance() {
@@ -173,7 +173,7 @@ var Singleton = function () {
       return instance;
     },
   };
-};
+})();
 
 const instance1 = Singleton.getInstance();
 const instance2 = Singleton.getInstance();
@@ -192,5 +192,142 @@ function toggle(...values) {
 const onOff = toggle("on", "off");
 console.log(onOff());
 
-
 //? Toggle functionality can be obtained by returning the next value cyclically on each call to the function
+
+//: Create a range function which return an array for the provided inputs as start and end
+
+function range(start, end) {
+  if (end === undefined) {
+    return function () {
+      return Range(start, end);
+    };
+  }
+
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+console.log(range(2, 6));
+
+//: Write a function which takes a function as  an argument to achive memoization
+
+//? Memoization is an optimization technique used primarily to speed up the programs by stroring the results of expresive calls and returning the cached result when the same input occur again.
+
+function memoize(fn) {
+  const cache = new Map();
+
+  return function () {
+    const key = JSON.stringify(arguments);
+    console.log(key);
+
+    if (cache.has(key)) {
+      console.log("Fetching cached...");
+      return cache.get(key);
+    } else {
+      cache.set(key, fn(...arguments));
+      return cache.get(key);
+    }
+  };
+}
+
+const factorial = memoize(function fact(value) {
+  return value > 1 ? value * fact(value - 1) : 1;
+});
+
+console.log(factorial(2));
+console.log(factorial(2));
+
+//: Create a single function which can perform sum(a,b,c), sum(a,b)(c), sum(a)(b,c) and sum(a)(b)(c) and returns sum of a,b and c
+
+function sum1(a, b, c) {
+  if (a !== undefined && b !== undefined && c !== undefined) {
+    return a + b + c;
+  }
+  if (a !== undefined && b !== undefined) {
+    return function (c) {
+      return sum1(a, b, c);
+    };
+  }
+  return function (b, c) {
+    if (b !== undefined && c !== undefined) {
+      return sum1(a, b, c);
+    }
+    return function (c) {
+      return sum1(a, b, c);
+    };
+  };
+}
+
+//: Design a function which can keep reciving the arguments on each function call and return the sum when no argument is passed.
+
+const summ = (a) => {
+  return function (b) {
+    if (b === undefined) {
+      return a;
+    }
+    return summ(a + b);
+  };
+};
+
+console.log(summ(1)(2)(3)(4)(5)());
+
+// 2
+
+const summm = (a) => (b) => b === undefined ? a : summ(a + b);
+
+console.log(summm(1)(2)(3)(4)(5)());
+
+//: Create an interface for a function such that whenever a function is tiggered the system should log the time. Do not modify the function code.
+
+function generateSecretObject(key, value) {
+  return { [key]: value };
+}
+
+generateSecretObject = new Proxy(generateSecretObject, {
+  apply(target, context, args) {
+    console.log(`${target.name} function is accessed at ${new Date()}`);
+    return target.apply(context, args);
+  },
+});
+
+// driver code
+const user = {
+  username: "0001",
+  generateSecretObject,
+};
+generateSecretObject("username", "Password"); // "generateSecretObject function is accessed at {time}"
+
+//: Create an interface exposing subcribe and publish functionality, which allows publishing data which in turn invokes all the subscribers with the data.
+
+function pubSub() {
+  const subscribers = [];
+
+  function publish(data) {
+    subscribers.forEach((subscriber) => subscriber(data));
+  }
+
+  function subscribe(fn) {
+    subscribers.push(fn);
+  }
+
+  return {
+    publish,
+    subscribe,
+  };
+}
+
+// driver code
+const pubSubObj = pubSub();
+pubSubObj.subscribe((data) => {
+  console.log("Subscriber 1: " + data);
+});
+pubSubObj.subscribe((data) => {
+  console.log("Subscriber 2: " + data);
+});
+
+// all subscribers will be called with the data on publish
+pubSubObj.publish("Value is 10");
