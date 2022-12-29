1a7ad422b82ca391b1dcf7eb1c6b35b270c9e985
therogersak <therogersak@gmail.com>
2022-12-29 13:01:36 +0530

    ✅ PostFix & PreFix

---
+++ Operators/PreFix_PostFix.js
@@ -0,0 +1,13 @@
+//: The Postfix and PreFix
+
+/* The increment operator (++) adds 1 to its operand and return a value. Similarly, the decrement operator (--) subtracts 1 from its operand and return a value. Both operators can be used either prefix(++i, --i) or postFix(i++, i--) */
+
+//* prefix
+
+let i = 0; // i = 0
+let j = ++i; // i = 1, j = 1
+let k = --i; // i = 0, k = 0
+
+let a = 0; // a = 0
+let b = a++; // b = 0; a = 1;
+let c = a--; // a = 0, c = 1
