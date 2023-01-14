61f387faba38c8acf51a1a0fa283799bad84cb80
therogersak <therogersak@gmail.com>
2023-01-14 20:21:08 +0530

    ✅ Execution Context

---
+++ Comman/Execution-Context.js
@@ -0,0 +1,27 @@
+//: JavaScript Execution Context
+
+/* All javascript code needs to be hosted and run in some kind of environment. In most case, that environment would be a web browser. */
+
+//* Parser
+
+/* A parser or syntax parser is a program that reads your code line-by-line. It understand how the code fits the syntax defined by the programing Language and what if (the code) is expected to do. */
+
+//* JavaScript Engine
+
+/* A javascript engine is simply a computer program that recives javascript source code and compiles it to the binary instruction (machine code) that a cpu can understand. Javascript engines are typically developed by web browser vendors, and each major browser has one. */
+
+//* Function Declarations
+
+/* These are function that are assigned a name */
+
+//* Function Expressions
+
+/* These are anonymouse functions, that is function without a function name like "js function () {}". They are usually used in statement, like assigning a function to a variable */
+
+let someValue = function () {
+  // statements
+};
+
+
+//* How Javascript Code Gets Executed
+
